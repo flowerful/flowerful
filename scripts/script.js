4ced0c0220a7364c9e0bc5fdfc9e813f2492b14a
spencermdavis <spencermdavis@mac.com>
2020-07-28 16:19:15 -0400

    started event listener

--- scripts/script.js
+++ scripts/script.js
@@ -1,17 +1,29 @@
 const plantApp = {};
 
+plantApp.token = "IqewWccMHBa3cnlYw2-TrtPrsxLhFmDY10kpsztBXk4";
+
+// submit button init listener
+plantApp.eventListener = () => {
+  $('form').on('submit', function(e) {
+    e.preventDefault();
+    $('form').attr('aria-label', 'submitted');
+    $('button[type=submit]').addClass('submitted');
+    const userColor = $("#plantColor").val();
+    if (isNaN(userColor) )
+  })
+}
+
+// plant displayer
+
 plantApp.getPlants = (color) => {
   // the information we provide to the function is a query so we name the parameter as such to make our code more human-legible
   $.ajax({
-    url: `https://trefle.io/api/v1/species?filter%5Bflower_color%5D=${color}&token=IqewWccMHBa3cnlYw2-TrtPrsxLhFmDY10kpsztBXk4`,
+    url: `https://trefle.io/api/v1/species?filter%5Bflower_color%5D=${color}&token=${plantApp.token}`,
     // query contains the user data from selected; will change the url to point to the specified season through the power of template literal
     method: "GET",
     dataType: "json",
   }).then(function (apiResults) {
-    // "when yr done with the data cann you call this function for me please javascript thank you javascript"
     plantApp.displayPlants(apiResults);
-    // queenApp.queens = apiResults;
-    // queenApp.displayQueens();
   });
 };
 
