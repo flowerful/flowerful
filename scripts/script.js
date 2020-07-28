fc6f8c2d3cf8be801d8b03e0cb07458e9612c818
spencermdavis <spencermdavis@mac.com>
2020-07-28 17:35:45 -0400

    api call and display function

--- scripts/script.js
+++ scripts/script.js
@@ -4,16 +4,32 @@ plantApp.token = "IqewWccMHBa3cnlYw2-TrtPrsxLhFmDY10kpsztBXk4";
 
 // submit button init listener
 plantApp.eventListener = () => {
-  $('form').on('submit', function(e) {
+  $("form").on("submit", function (e) {
     e.preventDefault();
-    $('form').attr('aria-label', 'submitted');
-    $('button[type=submit]').addClass('submitted');
+    $("form").attr("aria-label", "submitted");
+    $("button[type=submit]").addClass("submitted");
     const userColor = $("#plantColor").val();
-    if (isNaN(userColor) )
-  })
-}
+    if (isNaN(userColor)) {
+      $("#plantApp").find("input:text").val("");
+    } else {
+      plantApp.getPlants(userColor);
+    }
+  });
+};
 
-// plant displayer
+plantApp.displayPlants = (plants) => {
+  $("results").empty();
+  plants.data(function (plant) {
+    $("results").append(` 
+      <div class="plantCard">  
+        <h3> ${plants.data.common_name}</h3>
+        <div class="imageContainer"> 
+          <img src="${plants.data.image_url}" alt="${plants.data.common_name}" class="plantImage"/>
+        </div>
+      </div>
+    `);
+  });
+};
 
 plantApp.getPlants = (color) => {
   // the information we provide to the function is a query so we name the parameter as such to make our code more human-legible
